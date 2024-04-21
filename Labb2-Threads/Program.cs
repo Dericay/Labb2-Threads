3790a5ca108d5fb2685dfcbfdb2d1de7725e58f1
Anton Larsson <anton.cj.sk8@gmail.com>
2024-04-21 14:00:47 +0200

    Add project files.

---
+++ Labb2-Threads/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Labb2_Threads
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RaceCars Car1 = new RaceCars("RedBull", 120, 0);
+            RaceCars Car2 = new RaceCars("Ferrari", 120, 0);
+
+            RaceCars.cars.Add(Car1);
+            RaceCars.cars.Add(Car2);
+            
+
+            Thread rc1 = new Thread(() =>
+            {
+                Car1.startRace();
+                Console.WriteLine($"{Car1.Name} has finished the race!");
+            });
+
+            Thread rc2 = new Thread(() =>
+            {
+                Car2.startRace();
+                Console.WriteLine($"{Car2.Name} has finished the race!");
+            });
+
+            Thread rc3 = new Thread(() =>
+            {
+                RaceCars.InfoCars(RaceCars.GetAllCars());
+            });
+
+            rc1.Start();
+            rc2.Start();
+            rc3.Start();
+            
+            
+
+            
+
+
+        }
+    }
+}
---
+++ Labb2-Threads/RaceCars.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Threads
+{
+    internal class RaceCars
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Distance { get; set; }
+
+        public RaceCars(string name, int speed, int distance)
+        {
+            Name = name;
+            Speed = speed;
+            Distance = distance;
+        }
+
+        public static List<RaceCars> cars = new List<RaceCars>();
+
+        public static List<RaceCars> GetAllCars()
+        {
+            return cars;
+        }
+
+        public void OutOfGas()
+        {
+            Console.WriteLine($"{Name} is out of gas!");
+            Thread.Sleep(30000);
+        }
+
+        public void FlatTire()
+        {
+            Console.WriteLine($"{Name} has gotten a flat tire!");
+            Thread.Sleep(20000);
+        }
+
+        public void Bird()
+        {
+            Console.WriteLine($"{Name} got a bird on the windshield!");
+            Thread.Sleep(10000);
+        }
+        public void EngineFailure()
+        {
+            Console.WriteLine($"{Name} got an engine failure!!");
+            Speed -= 1;
+        }
+
+        public void events()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 50);
+            if(num == 1)
+            {
+                OutOfGas();
+            }
+            if(num == 20 || num == 40)
+            {
+                FlatTire();
+            }
+            if(num >= 13 && num <= 17)
+            {
+                Bird();
+            }
+            if(num >= 21 && num <= 30)
+            {
+                EngineFailure();
+            }
+
+        }
+
+        public void startRace()
+        {          
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            while (Distance <= 8000)
+            {
+                Distance += Speed;
+                Console.WriteLine($"{Name} Distance: {Distance}");
+                Thread.Sleep(1000);
+                if (stopwatch.Elapsed.TotalSeconds > 5)
+                {
+                    events();
+                    stopwatch.Restart();
+                }
+            }
+        }
+
+        public static void InfoCars(List<RaceCars> cars)
+        {
+            while (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                foreach (RaceCars c in cars)
+                {
+                    Console.WriteLine($"{c.Name} has driven {c.Distance} and speed is {c.Speed}kmh");
+                }
+            }
+        }
+    }
+}
